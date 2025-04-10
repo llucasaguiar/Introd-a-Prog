150bc33d82855d6df047bb1d9e9cfae4c0d6311b
Lucas Aguiar <llucasaguiar16@gmail.com>
2025-04-09 21:27:41 -0300

    Add files via upload

---
+++ A9E16for.py
@@ -0,0 +1,6 @@
+# Armazene os nomes de alguns de seus amigos em uma lista chamada amigos.
+# Exiba o nome de cada pessoa acessando cada elemento da lista um de cada vez.
+
+nomes_amigos = ['Lucas', 'Flávia', 'Gabriel', 'Arthur']
+for nome in nomes_amigos:
+    print(f'Nome é: {nome}.')
---
+++ A9E17for.py
@@ -0,0 +1,8 @@
+#Utilizando a lista do exercício anterior, exiba uma saudação
+# ("Olá como vai você"), personalizado com o nome de cada amigo.
+#A saudação deve ser a mesma, alterando apenas o nome do amigo.
+
+nomes_amigos = ['Sarah', 'Alex', 'Everton']
+
+for nome in nomes_amigos:
+    print(f'Olá {nome}, como você está?')
